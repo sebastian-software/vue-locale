f84a33c532e46c5c643cf2fa051d00c0043cfe80
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-11 12:13:33 +0100

    Worked on writing json data

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,10 +3,21 @@
 import gulp from "gulp";
 import del from "del";
 import extractData from 'formatjs-extract-cldr-data';
+import jsonfile from "jsonfile";
+import { map } from "lodash";
 
-var data = extractData({
-    locales    : ['en-US', 'en-GB'],
-    pluralRules: true
+gulp.task("build-data", function(done) {
+
+  var data = extractData({
+    pluralRules: true,
+    relativeFields: true
+  });
+
+  jsonfile.spaces = 0;
+
+  return Promise.all(map(data, function(value, locale) {
+    return new Promise(function(resolve, reject) {
+      jsonfile.writeFile("data/" + locale + ".json", value, resolve);
+    });
+  }));
 });
-
-console.log(data);
