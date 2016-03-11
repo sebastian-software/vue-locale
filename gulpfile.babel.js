c92699d2c18eaf78014af9cd1c56e23b80ac9f0e
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-11 12:16:32 +0100

    Worked on data extraction/cleanup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,23 +1,30 @@
-"use strict";
+"use strict"
 
-import gulp from "gulp";
-import del from "del";
-import extractData from 'formatjs-extract-cldr-data';
-import jsonfile from "jsonfile";
-import { map } from "lodash";
+import gulp from "gulp"
+import del from "del"
+import extractData from "formatjs-extract-cldr-data"
+import jsonfile from "jsonfile"
+import { map } from "lodash"
 
-gulp.task("build-data", function(done) {
+gulp.task("clean-data", function()
+{
+  return del(["data/*"]);
+})
 
-  var data = extractData({
+gulp.task("build-data", function()
+{
+  let data = extractData(
+  {
     pluralRules: true,
     relativeFields: true
-  });
+  })
 
-  jsonfile.spaces = 0;
+  jsonfile.spaces = 0
 
-  return Promise.all(map(data, function(value, locale) {
-    return new Promise(function(resolve, reject) {
-      jsonfile.writeFile("data/" + locale + ".json", value, resolve);
-    });
-  }));
-});
+  return Promise.all(map(data, (value, locale) =>
+  {
+    return new Promise((resolve, reject) => {
+      jsonfile.writeFile("data/" + locale + ".json", value, resolve)
+    })
+  }))
+})
