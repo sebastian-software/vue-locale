c6e40738175ec6315b5bf26cec29e08a1adbd0c0
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-13 18:53:13 +0100

    Tweaked export so that it contains use strict mode flag

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,7 +23,7 @@ gulp.task("build-data", function()
     return new Promise((resolve) =>
     {
       let stringified = stringify(value, null, "  ")
-      let result = `exports.default = ${stringified}`
+      let result = `"use strict";exports.default=${stringified};`
 
       fs.writeFile("data/" + locale + ".js", result, resolve)
     })
